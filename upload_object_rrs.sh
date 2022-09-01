70daa428b54370a109784f47051253b7a8d2dff6
Supawit Sippasuchon <supawitsip@gmail.com>
2022-09-01 03:32:17 -0700

    v1.2

--- upload_object_rrs.sh
+++ upload_object_rrs.sh
@@ -5,9 +5,9 @@ port="9001"
 ssl="false"
 access_key="minioadmin"
 secret_key="minioadmin"
-file_upload_path="/home/supawit/js/world.txt"
-file_name_in_minio="world.txt"
-js_path_name="/home/supawit/js/upload_file_world.js"
+file_upload_path="/home/supawit/js/100MB.zip"
+file_name_in_minio="100MB_3.zip"
+js_path_name="/home/supawit/js/upload_file_100mb_rrs.js"
 bucket_name="test3"
 
 cat<<EOF>$js_path_name
@@ -30,7 +30,11 @@ var fileStat = Fs.stat(file, function(err, stats) {
     return console.log(err)
   }
 
-  minioClient.putObject('$bucket_name', '$file_name_in_minio', fileStream, stats.size, function(err, objInfo) {
+  var metaData = {
+      'ContentType': 'application/octet-stream',
+      'X-Amz-Storage-Class': 'REDUCED_REDUNDANCY'
+  }
+  minioClient.putObject('$bucket_name', '$file_name_in_minio', fileStream, stats.size, metaData, function(err, objInfo) {
       if(err) {
           return console.log(err) // err should be null
       }
